74b5fe8ed8222a9f89520864e1192f5b030715ce
Kevin Yuan <kevihiiin@users.noreply.github.com>
2020-03-22 23:01:42 +0100

    Added mails and more details

--- apps/matcher/tasks.py
+++ apps/matcher/tasks.py
@@ -7,7 +7,6 @@ from email.mime.text import MIMEText
 # this method takes a list of inquiries and a list of helpers and returns a dictionary,
 # containing inquiry ids as keys and a list of helpers as values
 def match_all(inquiries, helpers):
-
     # initial setup for GeoDistance
     dist = pgeocode.GeoDistance('DE')
 
@@ -79,7 +78,60 @@ def match_indiviual(inquiries, helper):
     return None
 
 
-def send_mail(helper):
+def send_mail_participant(helper, institution):
+    template = """
+    Hallo {},\n
+    \n
+    herzlich willkommen bei Virushelfer.\n
+    Wir danken dir für dein Interesse.\n
+    {}\n
+    \n
+    \n
+    Hier haben wir einige Tipps für dich:\n
+    \n
+    Wenn du im Patientenkontakt arbeitest: Lass dir unbedingt eine Hygieneeinweisung vor Ort geben und lerne die Station kennen, um dann besser und sicherer arbeiten und helfen zu können. Versuche gerade solange nicht so viel zu tun ist, herauszufinden, wo auf deiner Station was liegt, um später schneller helfen zu können, wenn etwas benötigt wird.\n
+    \n
+    Tue nichts, womit du dir nicht sicher bist und spreche im Zweifelsfall das Pflegepersonal oder auch die Personalleitung an!\n
+    \n
+    Das genaue Anstellungsverhältnis ist sehr unterschiedlich, informiere dich bitte an deinem ersten Tag in deinem Krankenhaus, wie es mit Vergütung oder mit der Anerkennung als Praktikum für dein Studium oder ähnliches aussieht, wir hoffen, dass die Universitäten hier recht kulant sein werden.\n
+    \n
+    Am ersten Tag mitbringen:\n
+    - Impfnachweis (Masern, Tetanus, Hepatitis A;B, Diphtherie, Pertussis , Tetanus), wenn vorhanden\n
+    - ggf. aktuelle Immatrikulationsbescheinigung oder Nachweis der abgeschlossenen Berufsausbildung \n
+    - feste Schuhe\n
+    \n
+    Und auch an dieser Stelle noch einmal der Hinweis: solltest du eine der folgenden Gruppen angehören, teilt dies unbedingt vorab der Klinik mit:  \n
+    Aufenthalt in Hochrisikogebieten nach RKI in den letzten 2 Wochen \n
+    Kontakt zu CoVID19-Infizierten oder Verdachtsfällen in den letzten 2 Wochen \n
+    Zugehörigkeit zu einer Risikogruppe (Personen über 50 Jahre, Schwangere, Vorerkrankungen)\n
+    \n
+    Danke für deine Teilnahme :)\n
+    Schöne Grüße und bleib gesund,\n
+    dein Team von Virushelfer\n
+        """
+
+    hospital_notice = 'Leider benötigt gerade kein Krankenhaus deine Hilfe, aber wir melden uns bei dir, sobald wir ein Match für dich gefunden haben' if institution is None else f'Das folgende Krankenhaus benötigt deine Hilfe: {institution.name}'
+
+    message = template.format(f'{helper.first_name} {helper.last_name}', hospital_notice)
+    return send_mail(helper.e_mail, message)
+
+def send_mail_inquiry(institution):
+    template = """
+Sehr geehrte/r Herr/Frau {},
+
+vielen Dank für die Registrierung der Institution {}.
+
+Sobald Helfer gefunden wurden, die den Anforderungen entsprechen, werden wir Sie umgehend benachrichten.
+
+Bis dahin wünschen wir viel Erfolg im Kampf gegen COVID-19.
+
+Schöne Grüße
+das Virushelden Team
+    """
+    message = template.format(institution.last_name_contact, institution.name)
+    return send_mail(institution.e_mail, message)
+
+def send_mail(e_mail, message):
     # mail setup
     # set up the SMTP server
     s = smtplib.SMTP(host='mail.gmx.net', port=587)
@@ -88,12 +140,9 @@ def send_mail(helper):
 
     msg = MIMEMultipart()  # create a message
 
-    message = f"Hi {helper.first_name}, danke fürs Anmelden. Wenn du diese Mail bekommst, bsit du cool." \
-              f"Schöne Grüße, deine Virushelden"
-
     # setup the parameters of the message
     msg['From'] = 'virushelden@gmx.de'
-    msg['To'] = helper.e_mail
+    msg['To'] = e_mail
     msg['Subject'] = "Deine Anmeldung bei Virushelden"
 
     # add in the message body
@@ -105,4 +154,3 @@ def send_mail(helper):
 
     # Terminate the SMTP session and close the connection
     s.quit()
-
--- apps/matcher/views.py
+++ apps/matcher/views.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .models import Helper, Inquiry, Hospital
 from .forms import HelperForm, InquiryForm
-from .tasks import match_indiviual, send_mail
+from .tasks import match_indiviual, send_mail_inquiry, send_mail_participant
 
 
 class IndexView(generic.ListView):
@@ -16,6 +16,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Helper.objects.all()[:5]
+
 
 @csrf_exempt
 def helper_new(request):
@@ -26,6 +27,11 @@ def helper_new(request):
             # post.author = request.user
             # post.published_date = timezone.now()
             post.save()
+            # trying to match the helper to an inquiry
+            matched_inquiry = match_indiviual(Inquiry.objects.all(), post)
+            print("SENDING MAIL")
+            send_mail_participant(post, matched_inquiry)
+            print("SENT MAIL")
             return HttpResponseRedirect(reverse('matcher:helper_detail', kwargs={'helper_id': post.pk}))
     else:
         form = HelperForm()
@@ -33,14 +39,13 @@ def helper_new(request):
 
 
 def helper_detail(request, helper_id):
-    p = get_object_or_404(Helper, pk=helper_id)
-    # trying to match the helper to an inquiry
-    matched_inquiry = match_indiviual(Inquiry.objects.all(), p)
-    print("SENDING MAIL")
-    # send_mail(p)
-    print("SENT MAIL")
-    match_string = f'{matched_inquiry.id} at {matched_inquiry.hospital.name}' if matched_inquiry is not None else 'None'
-    return HttpResponse(f'{p.first_name} {p.last_name} was matched to the following Inquiry: ' + match_string)
+    person = get_object_or_404(Helper, pk=helper_id)
+    return render(request, 'polls/register_success.html', context={
+        'name': f'Liebe(r) {person.first_name} {person.last_name},',
+        'message': f'vielen Dank für dein Engagment!\nWir haben eine E-Mail an {person.e_mail} gesendet, in der Du genauere Informationen über das weitere Vorgehen erhälst.',
+        'button_style': 'style="background-color:#9AC082;border-color:#9AC082;color:#030202"'
+    })
+
 
 @csrf_exempt
 def institution_new(request):
@@ -51,6 +56,9 @@ def institution_new(request):
             # post.author = request.user
             # post.published_date = timezone.now()
             post.save()
+            print("SENDING MAIL")
+            send_mail_inquiry(post)
+            print("SENT MAIL")
             return HttpResponseRedirect(reverse('matcher:institution_detail', kwargs={'inquiry_id': post.pk}))
     else:
         form = InquiryForm()
@@ -58,10 +66,12 @@ def institution_new(request):
 
 
 def institution_detail(request, inquiry_id):
-    p = get_object_or_404(Hospital, pk=inquiry_id)
-    # trying to match the helper to an inquiry
-    matched_inquiry = match_indiviual(Inquiry.objects.all(), p)
-    return HttpResponse(f'{p.first_name} {p.last_name} was matched to the following Inquiry: {matched_inquiry.id} at {matched_inquiry.hospital.name}')
+    hospital = get_object_or_404(Hospital, pk=inquiry_id)
+    return render(request, 'polls/register_success.html', context={
+        'name': f'Sehr geeherter Herr / sehr geehrte Frau {hospital.last_name_contact},',
+        'message': f'vielen Dank für Ihre Anfrage. Wir haben eine E-Mail an {hospital.e_mail} gesendet, in der Sie weitere Informationen über das weitere Vorgehen erhalten.',
+        'button_style': 'style="background-color:#C88797;border-color:#C88797;color:#030202"'
+    })
 
 # class DetailView(generic.DetailView):
 #     model = Poll
