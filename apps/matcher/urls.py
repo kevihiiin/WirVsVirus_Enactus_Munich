7abc72191494a972ad9d8f50e90c06ac1ae0ddf5
Kevin Yuan <kevihiiin@users.noreply.github.com>
2020-03-22 19:58:51 +0100

    Removed Participant & Inquiry overview page

--- apps/matcher/urls.py
+++ apps/matcher/urls.py
@@ -6,10 +6,10 @@ from . import views
 
 app_name = "matcher"
 urlpatterns = [
-    path(r'helper/', views.IndexView.as_view(), name='helper_index'),
+    # path(r'helper/', views.IndexView.as_view(), name='helper_index'),
     path(r'helper/new/', views.helper_new, name='helper_new'),
     path(r'helper/detail/<uuid:helper_id>', views.helper_detail, name='helper_detail'),
-    path(r'institution/', views.IndexView.as_view(), name='institution_index'),
+    # path(r'institution/', views.IndexView.as_view(), name='institution_index'),
     path(r'institution/new/', views.institution_new, name='institution_new'),
     path(r'institution/detail/<uuid:inquiry_id>', views.institution_detail, name='institution_detail'),
     # url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
